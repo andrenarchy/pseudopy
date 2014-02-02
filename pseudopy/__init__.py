e92f5e7c895d7f8d2e8b604ffe6a34d08bdda8cd
André Gaul <gaul@web-yard.de>
2014-02-02 16:24:41 +0100

    major cleanup after migration to classes

--- pseudopy/__init__.py
+++ pseudopy/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import compute, demo, visualize
 from .nonnormal import NonnormalMeshgrid, NonnormalTriang, NonnormalPoints
 from .normal import Normal, NormalEvals
+from . import demo
 
 __all__ = ['NonnormalMeshgrid', 'NonnormalTriang', 'NonnormalPoints',
            'Normal', 'NormalEvals',
-           'compute', 'demo', 'visualize']
+           'demo']
