7dbcb3cd1f7c0681d0dce7c4df76a05a882dd753
André Gaul <gaul@web-yard.de>
2014-02-02 16:32:08 +0100

    bump version

--- setup.py
+++ setup.py
@@ -9,14 +9,15 @@ def read(fname):
 
 setup(name='pseudopy',
       packages=['pseudopy'],
-      version='1.0.0',
+      version='1.1.0',
       description='Compute and visualize pseudospectra of'
                   + ' matrices (like eigtool)',
       long_description=read('README.md'),
       author='André Gaul',
       author_email='gaul@web-yard.de',
       url='https://github.com/andrenarchy/pseudopy',
-      requires=['scipy (>=0.12)', 'matplotlib (>=1.2.1)'],
+      requires=['matplotlib (>=1.2)', 'numpy (>=1.7)',
+                'scipy (>=0.12)', 'shapely (>=1.2)'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Science/Research',
