3c08e6946ae1933459c25f2def5eb2ef8e65d3f3
André Gaul <gaul@web-yard.de>
2014-04-07 23:20:09 +0200

    [init] add NonnormalAuto

--- pseudopy/__init__.py
+++ pseudopy/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .nonnormal import NonnormalMeshgrid, NonnormalMeshgridAuto, \
-    NonnormalTriang, NonnormalPoints
+    NonnormalTriang, NonnormalPoints, NonnormalAuto
 from .normal import Normal, NormalEvals
 from . import demo, utils
 
 __all__ = ['NonnormalMeshgrid', 'NonnormalMeshgridAuto', 'NonnormalTriang',
-           'NonnormalPoints',
+           'NonnormalPoints', 'NonnormalAuto',
            'Normal', 'NormalEvals',
            'demo', 'utils']
